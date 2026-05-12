bab8a0c3edd0c6bf9dcfecff795337104ad3942d
notSaqlain <iamnotsaqlain@gmail.com>
2026-05-12 09:36:45 +0200

    initialize files

---
+++ src/main/java/com/avbo/northwind_entity_manager/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.avbo.northwind_entity_manager;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
